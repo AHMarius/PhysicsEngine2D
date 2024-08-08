e4ac13271c915c7a8b55b85bf958b8e4b27c6af5
Alex Marius H. <hritcualex862@gmail.com>
2024-08-08 23:25:55 +0300

    -Circle interaction

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,5 +1,6 @@
 #include <raylib.h>
 #include <iostream>
+#include <cmath>
 //Constants
 const bool DEBUG_MODE = true;
 const int SCREEN_WIDTH = 800;
@@ -9,6 +10,7 @@ const float LINE_THICKNESS = 2.0f;
 
 //Objects
 struct GameObject {
+    unsigned short Type; //0-circle, 1 rectangle
     struct {
         Vector2 Position;
         Vector2 Size;
@@ -38,16 +40,23 @@ bool ObjectIntersection(GameObject ObjA, GameObject ObjB)
     if (ObjA.AABB.max.y < ObjB.AABB.min.y || ObjA.AABB.min.y > ObjB.AABB.max.y) return false;
     return true;
 }
+bool ObjectInteractionCircles(GameObject ObjA, GameObject ObjB)
+{
+    int R = ObjA.Transform.Size.x + ObjB.Transform.Size.x;
+    R *= R;
+    return R < pow(ObjA.Transform.Position.x + ObjB.Transform.Position.x, 2) + pow(ObjA.Transform.Position.y + ObjB.Transform.Position.y, 2);
+}
 int main() {
     //Declarations
     GameObject Obj[10];
         //First Object
-    Obj[0].Transform.Position = Vector2{50, 0};
-    Obj[0].Transform.Size = Vector2{100,100};
+    Obj[0].Transform.Position = Vector2{50, 151};
+    Obj[0].Transform.Size = Vector2{50,50};
+    Obj[0].Type = 0;
         //First Object
     Obj[1].Transform.Position = Vector2{ 200, 50 };
-    Obj[1].Transform.Size = Vector2{ 100,100 };
-
+    Obj[1].Transform.Size = Vector2{ 50,50 };
+    Obj[1].Type = 0;
     //Debugging
     for (int counter = 0; counter < OBJECT_NUMBER && DEBUG_MODE; counter++)
     {
@@ -64,24 +73,43 @@ int main() {
         //AABB Calculations
         for (int counter = 0; counter < OBJECT_NUMBER; counter++)
         {
-            Obj[counter].toAABB();
+            if (Obj[counter].Type != 0)
+                Obj[counter].toAABB();
         }
         //Intersection Check
-        bool interaction = ObjectIntersection(Obj[0], Obj[1]);
+        bool InteractionCheck = false;
+        if (Obj[0].Type == 0)
+        {
+            ObjectInteractionCircles(Obj[0], Obj[1]);
+        }
+        else {
+            InteractionCheck = ObjectIntersection(Obj[0], Obj[1]);
+        }
         BeginDrawing();
         ClearBackground(LIGHTGRAY); 
         //Draw Objects
         for (int counter = 0; counter < OBJECT_NUMBER; counter++)
         {
-            DrawRectangleV(Obj[counter].Transform.Position, Obj[counter].Transform.Size, (!interaction)?GREEN:RED);
-
+            if (Obj[counter].Type == 0)
+            {
+                DrawCircleV(Obj[counter].Transform.Position, Obj[counter].Transform.Size.x, (!InteractionCheck) ? GREEN : RED);
+            }
+            else {
+                DrawRectangleV(Obj[counter].Transform.Position, Obj[counter].Transform.Size, (!InteractionCheck) ? GREEN : RED);
+            }
         }
         //Draw Hitboxes
         for (int counter = 0; counter < OBJECT_NUMBER && DEBUG_MODE; counter++)
         {
             GameObject varObj = Obj[counter];
-            DrawLineEx(varObj.AABB.min, varObj.AABB.max, LINE_THICKNESS, RED);
-            DrawRectangleLines(varObj.Transform.Position.x, varObj.Transform.Position.y, varObj.Transform.Size.x, varObj.Transform.Size.y, RED);
+            if (varObj.Type == 0) {
+                DrawCircleLinesV(varObj.Transform.Position, varObj.Transform.Size.x, RED);
+                DrawLine(varObj.Transform.Position.x - varObj.Transform.Size.x, varObj.Transform.Position.y, varObj.Transform.Position.x + varObj.Transform.Size.x, varObj.Transform.Position.y, RED);
+            }
+            else {
+                DrawLineEx(varObj.AABB.min, varObj.AABB.max, LINE_THICKNESS, RED);
+                DrawRectangleLines(varObj.Transform.Position.x, varObj.Transform.Position.y, varObj.Transform.Size.x, varObj.Transform.Size.y, RED);
+            }
         }
         EndDrawing();
     }
