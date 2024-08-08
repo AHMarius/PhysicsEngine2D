37c53bc476083d6a04cfb6ca6c1afe86c9f23811
Alex Marius H. <hritcualex862@gmail.com>
2024-08-08 20:10:17 +0300

    Added Debug Function for checking the AABB positions

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,34 +1,38 @@
 #include <raylib.h>
-
+#include <iostream>
+//Constants
+const int screenWidth = 800;
+const int screenHeight = 600;
+//Objects
+struct GameObject {
+    struct {
+        Vector2 Position;
+        Vector2 Size;
+        Vector3 Rotation;
+    }Transform;
+    //Axis Aligned Bounding Boxes
+    struct {
+        Vector2 min;
+        Vector2 max;
+    }AABB;
+    void toAABB()
+    {
+        AABB.min = Transform.Position;
+        AABB.max = Vector2{ Transform.Size.x - Transform.Position.x, Transform.Size.y - Transform.Position.y };
+    }
+};
+//Functions
+    //Debug
+void DebugAABB(GameObject Obj)
+{
+    std::cout << Obj.AABB.min.x << " " << Obj.AABB.min.y << " " << Obj.AABB.max.x << " " << Obj.AABB.max.y;
+}
+    //Returnable
 int main() {
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-
-
-    //Objects
-    struct GameObject {
-        struct {
-            Vector2 Position;
-            Vector2 Size;
-            Vector3 Rotation;
-        }Transform;
-        //Axis Aligned Bounding Boxes
-        struct {
-            Vector2 min;
-            Vector2 max;
-        }AABB;
-        void toAABB()
-        {
-            AABB.min = Transform.Position;
-            AABB.max = Vector2{ Transform.Size.x - Transform.Position.x, Transform.Size.y - Transform.Position.y };
-        }
-    };
-    //Functions
-
     //Declarations
-    GameObject Ball;
-    Ball.Transform.Position = Vector2{ 100, 0 };
-    Ball.Transform.Size = Vector2{ 100,100 };
+    GameObject Obj;
+    Obj.Transform.Position = Vector2{ 100, 0 };
+    Obj.Transform.Size = Vector2{ 100,100 };
     //Raylib loop
     InitWindow(screenWidth, screenHeight, "Raylib Program");
     SetTargetFPS(60);
@@ -36,7 +40,7 @@ int main() {
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(LIGHTGRAY); // Dark Green
-        DrawRectangleV(Ball.Transform.Position, Ball.Transform.Size, GREEN);
+        DrawRectangleV(Obj.Transform.Position, Obj.Transform.Size, GREEN);
         EndDrawing();
     }
 
