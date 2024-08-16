a0994abf74d0e3e2e310eb369f72ad4c26c898dd
Alex Marius H. <100028783+AHMarius@users.noreply.github.com>
2024-08-16 13:45:20 +0300

    Rollback (fixed bugs) -Code clean-up

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,12 +3,13 @@
 #include <raylib.h>
 
 // Constants
-const bool DEBUG_MODE = false;
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-
-const int FPS_LIMIT = 60;
-const int VECTOR_LENGHT = 60;
+const bool DEBUG_MODE = 1;
+const unsigned short SCREEN_WIDTH = 800;
+const unsigned short SCREEN_HEIGHT = 600;
+const bool SPAWN_OBJECT_STATIC_TYPE = 0;
+const bool SPAWN_OBJECT_VARIABLE_TYPE = 0;
+const unsigned short FPS_LIMIT = 60;
+const unsigned short VECTOR_LENGHT = 60;
 const float LINE_THICKNESS = 2.0f;
 const float GIZMOS_SIZE = 2.0f;
 
@@ -28,6 +29,7 @@ struct GameObject {
 		Vector2 EndPoint;
 		float Restitution;
 		float Mass;
+		float InvMass;
 	} RigidBody;
 
 	struct {
@@ -39,14 +41,37 @@ struct GameObject {
 		AABB.min = Transform.Position;
 		AABB.max = Vector2{ Transform.Size.x + Transform.Position.x, Transform.Size.y + Transform.Position.y };
 	}
+	void CalculateVelocity() {
+		float a = RigidBody.EndPoint.x, x = Transform.Position.x;
+		float b = RigidBody.EndPoint.y, y = Transform.Position.y;
+
+		Vector2 direction = Vector2{ a - x, b - y };
+
+		float length = sqrt(direction.x * direction.x + direction.y * direction.y);
+		if (length != 0) {
+			direction = Vector2{ direction.x / length, direction.y / length };
+		}
+		RigidBody.Velocity = Vector2{ direction.x, direction.y };
+	}
+
+	void MoveTowards()
+	{
+		if (RigidBody.EndPoint.x == -1 || RigidBody.EndPoint.y == -1) {
+			return;
+		}
+		float distanceToEndPoint = sqrt(pow(RigidBody.EndPoint.x - Transform.Position.x, 2) +
+			pow(RigidBody.EndPoint.y - Transform.Position.y, 2));
+
+		if (distanceToEndPoint < 1.0f) {
+			RigidBody.Velocity = Vector2{ 0, 0 };
+			RigidBody.EndPoint = Vector2{ -1, -1 };
+		}
+		else if (!Collided) {
+			CalculateVelocity();
+		}
+
+	}
 };
-
-// Functions
-
-// Debugging function
-void DebugAABB(GameObject Obj) {
-	std::cout << Obj.AABB.min.x << " " << Obj.AABB.min.y << " " << Obj.AABB.max.x << " " << Obj.AABB.max.y;
-}
 
 void ObjectInteraction(GameObject& ObjA, GameObject& ObjB) {
 	bool interaction = false;
@@ -64,18 +89,6 @@ void ObjectInteractionCircles(GameObject& ObjA, GameObject& ObjB) {
 	ObjA.Collided = ObjB.Collided = interaction;
 }
 
-void CalculateVelocity(GameObject& ObjA) {
-	float a = ObjA.RigidBody.EndPoint.x, x = ObjA.Transform.Position.x;
-	float b = ObjA.RigidBody.EndPoint.y, y = ObjA.Transform.Position.y;
-
-	Vector2 direction = Vector2{ a - x, b - y };
-
-	float length = sqrt(direction.x * direction.x + direction.y * direction.y);
-	if (length != 0) {
-		direction = Vector2{ direction.x / length, direction.y / length };
-	}
-	ObjA.RigidBody.Velocity = Vector2{ direction.x, direction.y };
-}
 
 void ResolveCollision(GameObject& ObjA, GameObject& ObjB) {
 	Vector2 relativeVelocity = Vector2{ ObjB.RigidBody.Velocity.x - ObjA.RigidBody.Velocity.x,
@@ -94,15 +107,15 @@ void ResolveCollision(GameObject& ObjA, GameObject& ObjB) {
 	float e = std::min(ObjA.RigidBody.Restitution, ObjB.RigidBody.Restitution);
 
 	float j = -(1 + e) * velocityAlongNormal;
-	j /= (1 / ObjA.RigidBody.Mass) + (1 / ObjB.RigidBody.Mass);
+	j /= ObjA.RigidBody.InvMass + ObjB.RigidBody.InvMass;
 
 	Vector2 impulse = Vector2{ j * normalVector.x, j * normalVector.y };
 
-	ObjA.RigidBody.Velocity = Vector2{ ObjA.RigidBody.Velocity.x - (impulse.x / ObjA.RigidBody.Mass),
-									  ObjA.RigidBody.Velocity.y - (impulse.y / ObjA.RigidBody.Mass) };
+	ObjA.RigidBody.Velocity = Vector2{ ObjA.RigidBody.Velocity.x - (impulse.x * ObjA.RigidBody.InvMass),
+									  ObjA.RigidBody.Velocity.y - (impulse.y * ObjA.RigidBody.InvMass) };
 
-	ObjB.RigidBody.Velocity = Vector2{ ObjB.RigidBody.Velocity.x + (impulse.x / ObjB.RigidBody.Mass),
-									  ObjB.RigidBody.Velocity.y + (impulse.y / ObjB.RigidBody.Mass) };
+	ObjB.RigidBody.Velocity = Vector2{ ObjB.RigidBody.Velocity.x + (impulse.x * ObjB.RigidBody.InvMass),
+									  ObjB.RigidBody.Velocity.y + (impulse.y * ObjB.RigidBody.InvMass) };
 }
 
 void CreateGameObject(int& OBJECT_NUMBER, GameObject ObjList[], Vector2 Position = Vector2{ 0, 0 },
@@ -118,28 +131,18 @@ void CreateGameObject(int& OBJECT_NUMBER, GameObject ObjList[], Vector2 Position
 	ObjList[OBJECT_NUMBER].RigidBody.ObjectSpeed = ObjectSpeed;
 	ObjList[OBJECT_NUMBER].RigidBody.EndPoint = EndPoint;
 	ObjList[OBJECT_NUMBER].RigidBody.Mass = Mass;
+	ObjList[OBJECT_NUMBER].RigidBody.InvMass = (Mass == 0) ? Mass : 1 / Mass;
 	ObjList[OBJECT_NUMBER].RigidBody.Restitution = Restitution;
 	ObjList[OBJECT_NUMBER].Collided = false;
+
 	OBJECT_NUMBER++;
 }
-
-int main() {
-	// Declarations
-	int OBJECT_NUMBER = 0;
-
-	// Objects
-	GameObject Obj[10];
-	//CreateGameObject(OBJECT_NUMBER, Obj, Vector2{ 50, 50 }, Vector3{ 0, 0, 0 }, Vector2{ 25, 25 }, 0, 2, Vector2{ 500, 500 }, 1, 0.5f);
-	//CreateGameObject(OBJECT_NUMBER, Obj, Vector2{ 100, 180 }, Vector3{ 0, 0, 0 }, Vector2{ 25, 25 }, 0, 2, Vector2{ 400, 400 }, 1, 0.5f);
-
+void SetupLogic(int& OBJECT_NUMBER, GameObject Obj[10])
+{
+	Vector2 StartingPoints[100], EndPoints[100], BasicPoints[100];
+	int StartPointNumber = 0, EndPointNumber = 0, BasicPointNumber = 0;
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Raylib Program");
 	SetTargetFPS(FPS_LIMIT);
-	// Debugging
-	for (int counter = 0; counter < OBJECT_NUMBER && DEBUG_MODE; counter++) {
-		DebugAABB(Obj[counter]);
-	}
-	Vector2 StartingPoints[10], EndPoints[10], BasicPoints[10];
-	int StartPointNumber = 0, EndPointNumber = 0, BasicPointNumber = 0;
 	while (IsKeyUp(KEY_SPACE))
 	{
 		if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
@@ -181,138 +184,134 @@ int main() {
 	EndDrawing();
 	for (int i = 0; i < EndPointNumber; i++)
 	{
-		CreateGameObject(OBJECT_NUMBER, Obj, StartingPoints[i], Vector3{ 0, 0, 0 }, Vector2{ 25, 25 }, 0, 3, EndPoints[i], 1, 0.5f);
+		CreateGameObject(OBJECT_NUMBER, Obj, StartingPoints[i], Vector3{ 0, 0, 0 }, Vector2{ 25, 50 }, 1, 3, EndPoints[i], 1, 0.5f);
 	}
 	for (int i = EndPointNumber; i < StartPointNumber; i++)
 	{
-		CreateGameObject(OBJECT_NUMBER, Obj, StartingPoints[i], Vector3{ 0, 0, 0 }, Vector2{ 25, 25 }, 0, 3, StartingPoints[i], 1, 0.5f);
+		CreateGameObject(OBJECT_NUMBER, Obj, StartingPoints[i], Vector3{ 0, 0, 0 }, Vector2{ 25, 50 }, 1, 3, StartingPoints[i], 1, 0.5f);
 	}
 	for (int i = 0; i < BasicPointNumber; i++)
 	{
 		CreateGameObject(OBJECT_NUMBER, Obj, BasicPoints[i], Vector3{ 0, 0, 0 }, Vector2{ 25, 25 }, 0, 3, BasicPoints[i], 1, 0.5f);
 	}
-	// Raylib Loop
-	while (!WindowShouldClose()) {
-		// Update
-// Reset the collision check
-		for (int counter = 0; counter < OBJECT_NUMBER; counter++) {
-			Obj[counter].Collided = false;
-		}
+	CloseWindow();
+}
 
-		// Intersection Check
-		for (int i = 0; i < OBJECT_NUMBER; i++) {
-			for (int j = i + 1; j < OBJECT_NUMBER; j++) {
+void CollisionLogic(GameObject Obj[], int OBJECT_NUMBER)
+{
+	for (int counter = 0; counter < OBJECT_NUMBER; counter++) {
+		Obj[counter].Collided = false;
+		if (Obj[counter].Type != 0) {
+			Obj[counter].toAABB();
+		}
+	}
+
+	// Intersection Check
+	for (int i = 0; i < OBJECT_NUMBER; i++) {
+		for (int j = i + 1; j < OBJECT_NUMBER; j++) {
+			if (Obj[i].Type == 0 && Obj[j].Type == 0) {
+				ObjectInteractionCircles(Obj[i], Obj[j]);
+			}
+			else {
+				ObjectInteraction(Obj[i], Obj[j]);
+			}
+		}
+	}
+
+
+	//Velocity Calculations loop
+	for (int i = 0; i < OBJECT_NUMBER; i++) {
+		// Check for collisions and resolve them
+		for (int j = 0; j < OBJECT_NUMBER; j++) {
+			if (i != j) {
+				// Handle collisions between different object types
 				if (Obj[i].Type == 0 && Obj[j].Type == 0) {
+					// Both are circles
 					ObjectInteractionCircles(Obj[i], Obj[j]);
 				}
 				else {
+					// At least one is a rectangle
 					ObjectInteraction(Obj[i], Obj[j]);
 				}
-			}
-		}
 
-
-		// AABB & Velocity Calculations loop
-		for (int counter = 0; counter < OBJECT_NUMBER; counter++) {
-			// Ensure AABB is updated for non-circle objects
-			if (Obj[counter].Type != 0) {
-				Obj[counter].toAABB();
-			}
-
-			// Reset the collision status
-			Obj[counter].Collided = false;
-
-			// Check for collisions and resolve them
-			for (int counter2 = 0; counter2 < OBJECT_NUMBER; counter2++) {
-				if (counter != counter2) {
-					// Handle collisions between different object types
-					if (Obj[counter].Type == 0 && Obj[counter2].Type == 0) {
-						// Both are circles
-						ObjectInteractionCircles(Obj[counter], Obj[counter2]);
-					}
-					else {
-						// At least one is a rectangle
-						ObjectInteraction(Obj[counter], Obj[counter2]);
-					}
-
-					// Resolve collisions if there is any
-					if (Obj[counter].Collided || Obj[counter2].Collided) {
-						ResolveCollision(Obj[counter], Obj[counter2]);
-					}
-				}
-			}
-
-			// If the object has collided, reset its EndPoint
-			if (Obj[counter].Collided) {
-				Obj[counter].RigidBody.EndPoint = Vector2{ -1, -1 };
-			}
-
-			// Handle movement towards EndPoint if not collided
-			if (Obj[counter].RigidBody.EndPoint.x != -1 && Obj[counter].RigidBody.EndPoint.y != -1) {
-				float distanceToEndPoint = sqrt(pow(Obj[counter].RigidBody.EndPoint.x - Obj[counter].Transform.Position.x, 2) +
-					pow(Obj[counter].RigidBody.EndPoint.y - Obj[counter].Transform.Position.y, 2));
-
-				if (distanceToEndPoint < 1.0f) {
-					Obj[counter].RigidBody.Velocity = Vector2{ 0, 0 };
-					Obj[counter].RigidBody.EndPoint = Vector2{ -1, -1 };
-				}
-				else if (!Obj[counter].Collided) {
-					CalculateVelocity(Obj[counter]);
+				// Resolve collisions if there is any
+				if (Obj[i].Collided && Obj[j].Collided) {
+					Obj[i].RigidBody.EndPoint = Vector2{ -1, -1 };
+					Obj[j].RigidBody.EndPoint = Vector2{ -1, -1 };
+					ResolveCollision(Obj[i], Obj[j]);
 				}
 			}
 		}
+		Obj[i].MoveTowards();
+	}
+}
+void PositionUpdate(GameObject Obj[], int OBJECT_NUMBER) {
 
-
-		// Position Change Loop
-		for (int counter = 0; counter < OBJECT_NUMBER; counter++) {
-			Obj[counter].Transform.Position = Vector2{
-				Obj[counter].Transform.Position.x + Obj[counter].RigidBody.Velocity.x * Obj[counter].RigidBody.ObjectSpeed,
-				Obj[counter].Transform.Position.y + Obj[counter].RigidBody.Velocity.y * Obj[counter].RigidBody.ObjectSpeed
-			};
+	// Position Change Loop
+	for (int counter = 0; counter < OBJECT_NUMBER; counter++) {
+		Obj[counter].Transform.Position = Vector2{
+			Obj[counter].Transform.Position.x + Obj[counter].RigidBody.Velocity.x * Obj[counter].RigidBody.ObjectSpeed,
+			Obj[counter].Transform.Position.y + Obj[counter].RigidBody.Velocity.y * Obj[counter].RigidBody.ObjectSpeed
+		};
+	}
+}
+void DrawScreenLogic(GameObject Obj[], int OBJECT_NUMBER)
+{
+	// Draw
+	BeginDrawing();
+	ClearBackground(BLACK);
+	// Draw Objects
+	for (int counter = 0; counter < OBJECT_NUMBER; counter++) {
+		if (Obj[counter].Type == 0) {
+			DrawCircleV(Obj[counter].Transform.Position, Obj[counter].Transform.Size.x, (!Obj[counter].Collided) ? DARKGREEN : RED);
+		}
+		else {
+			DrawRectangleV(Obj[counter].Transform.Position, Obj[counter].Transform.Size, (!Obj[counter].Collided) ? DARKGREEN : RED);
 		}
 
-		// Draw
-		BeginDrawing();
-		ClearBackground(BLACK);
+		// Draw velocity vector
+		Vector2 velocityEndPoint = Vector2{
+			Obj[counter].Transform.Position.x + Obj[counter].RigidBody.Velocity.x * VECTOR_LENGHT,
+			Obj[counter].Transform.Position.y + Obj[counter].RigidBody.Velocity.y * VECTOR_LENGHT
+		};
 
-		// Draw Objects
-		for (int counter = 0; counter < OBJECT_NUMBER; counter++) {
-			if (Obj[counter].Type == 0) {
-				DrawCircleV(Obj[counter].Transform.Position, Obj[counter].Transform.Size.x, (!Obj[counter].Collided) ? DARKGREEN : RED);
-			}
-			else {
-				DrawRectangleV(Obj[counter].Transform.Position, Obj[counter].Transform.Size, (!Obj[counter].Collided) ? DARKGREEN : RED);
-			}
-
-			// Draw velocity vector
-			Vector2 velocityEndPoint = Vector2{
-				Obj[counter].Transform.Position.x + Obj[counter].RigidBody.Velocity.x * VECTOR_LENGHT,
-				Obj[counter].Transform.Position.y + Obj[counter].RigidBody.Velocity.y * VECTOR_LENGHT
-			};
-
-			DrawLineEx(Obj[counter].Transform.Position, velocityEndPoint, LINE_THICKNESS, BLUE);
-		}
-
-		// Draw Hitboxes
-		for (int counter = 0; counter < OBJECT_NUMBER && DEBUG_MODE; counter++) {
-			GameObject varObj = Obj[counter];
-			if (varObj.Type == 0) {
-				DrawCircleLinesV(varObj.Transform.Position, varObj.Transform.Size.x, RED);
-				DrawLine(varObj.Transform.Position.x - varObj.Transform.Size.x, varObj.Transform.Position.y, varObj.Transform.Position.x + varObj.Transform.Size.x, varObj.Transform.Position.y, RED);
-			}
-			else {
-				DrawLineEx(varObj.AABB.min, varObj.AABB.max, LINE_THICKNESS, RED);
-				DrawRectangleLines(varObj.Transform.Position.x, varObj.Transform.Position.y, varObj.Transform.Size.x, varObj.Transform.Size.y, RED);
-			}
-			if (varObj.RigidBody.EndPoint.x != -1) {
-				DrawCircleV(varObj.RigidBody.EndPoint, GIZMOS_SIZE, YELLOW);
-				DrawLineV(varObj.Transform.Position, varObj.RigidBody.EndPoint, YELLOW);
-			}
-		}
-
-		EndDrawing();
+		DrawLineEx(Obj[counter].Transform.Position, velocityEndPoint, LINE_THICKNESS, BLUE);
 	}
 
+	// Draw Hitboxes
+	for (int counter = 0; counter < OBJECT_NUMBER && DEBUG_MODE; counter++) {
+		GameObject varObj = Obj[counter];
+		if (varObj.Type == 0) {
+			DrawCircleLinesV(varObj.Transform.Position, varObj.Transform.Size.x, RED);
+			DrawLine(varObj.Transform.Position.x - varObj.Transform.Size.x, varObj.Transform.Position.y, varObj.Transform.Position.x + varObj.Transform.Size.x, varObj.Transform.Position.y, RED);
+		}
+		else {
+			DrawLineEx(varObj.AABB.min, varObj.AABB.max, LINE_THICKNESS, RED);
+			DrawRectangleLines(varObj.Transform.Position.x, varObj.Transform.Position.y, varObj.Transform.Size.x, varObj.Transform.Size.y, RED);
+		}
+		if (varObj.RigidBody.EndPoint.x != -1) {
+			DrawCircleV(varObj.RigidBody.EndPoint, GIZMOS_SIZE, YELLOW);
+			DrawLineV(varObj.Transform.Position, varObj.RigidBody.EndPoint, YELLOW);
+		}
+	}
+
+	EndDrawing();
+}
+int main() {
+	// Declarations
+	int OBJECT_NUMBER = 0;
+	GameObject Obj[100];
+
+
+	SetupLogic(OBJECT_NUMBER, Obj);
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Raylib Program");
+	SetTargetFPS(FPS_LIMIT);
+	// Raylib Loop
+	while (!WindowShouldClose()) {
+		CollisionLogic(Obj, OBJECT_NUMBER);
+		PositionUpdate(Obj, OBJECT_NUMBER);
+		DrawScreenLogic(Obj, OBJECT_NUMBER);
+	}
 	CloseWindow();
 	return 0;
 }
